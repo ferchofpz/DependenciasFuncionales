dd6b09113f976ae5b1883ee6f43463995c149f81
Fernando Perez <ferchofpz@hotmail.com>
2018-09-22 16:36:56 -0500

    first commit

---
+++ Funciones - copia.py
@@ -0,0 +1,256 @@
+import json
+import itertools
+from operator import itemgetter
+
+t = []
+
+def getInputFromFile(route):
+    file = open(route, 'r');
+    file_content = json.loads(' '.join(file.readlines()))
+    file.close();    
+    return file_content
+
+def selectTypeInput():   
+    try:      
+        print("  (1) Ingresar ruta de archivo JSON \n"+
+              "  (2) Ingresar Manualmente la información", end="")
+        option = int(input("Ingrese la opción: "))
+        return option
+    except ValueError:
+        print("El valor ingresado no es un entero")
+        
+def getAttributes(jsonInput):
+    return jsonInput['T']
+
+def getDeterminants(l):                
+
+        determinants = []
+        for item in l:
+            #Crea arreglo con determinantes 
+            determinant = item.split('->')[0]
+            determinant = determinant.split(',')
+            #Crea arreglo con determinados
+            determinated = item.split('->')[1]
+            determinated = determinated.split(',')
+            #Organiza los determinantes
+            determinant = sorted(determinant)
+            #Organiza los determinados
+            determinated = sorted(determinated)
+            
+            #Convierte en una cadena de strings los determinantes
+            determinantTxt = ",".join(determinant)
+            #Convierte en una cadena de strings los determinados
+            determinatedTxt = ",".join(determinated)
+            
+            #Valida si una DF es trivial
+            if (determinatedTxt in determinantTxt) or (determinatedTxt == determinantTxt):                        
+               print("La DF "+item+" es trivial y se elimina")
+            else:
+                #Si la DF no es trivial la adiciona al arreglo determinants
+                element = { "determinant": determinant, "determinated": determinated}
+                determinant_list = element["determinant"]
+                if len(determinant_list) > 1:
+                    determinant_list = sorted(determinant_list)
+                    element['determinant'] = determinant_list
+                determinants.append(element)
+        determinants = sorted(determinants, key=itemgetter('determinant'))
+        
+        return determinants
+
+def calculateMinimalCoating(determinants):
+        resultStr = ''
+        #print(json.dumps(determinants, indent=4, sort_keys=True))
+        converted_determinants = []
+        for det in determinants:
+            if len(det['determinated']) > 1:
+                #print('get determinant with len equal to 1')
+                splitDeterminantsWithLenGreaterThan1(det, converted_determinants)
+            else:
+                converted_determinants.append(det)
+                
+        #print(converted_determinants)
+        converted_determinants = sorted(converted_determinants, key=lambda item : len(item['determinant']), reverse=True)
+        #print('\n DETERMINANTS STEP 1: \n ', converted_determinants, '\n')
+        resultStr = resultStr + '\n'
+        resultStr = resultStr + '\n'.join(str(i) for i in converted_determinants) 
+
+        l2 = []
+        attributesCombinations = []
+        #Find strange objects
+        for det in converted_determinants:
+            #print(len(det['determinant']))
+            #print(det)
+            resultStr = resultStr + str(len(det['determinant']))
+            resultStr = resultStr + '\n'.join(str(i) for i in det)
+
+            if len(det['determinant']) == 2 :                
+                
+                temp_determinant = []
+                for j in det['determinant']:
+                    closure = getClosure([j], converted_determinants.copy())
+                    contained = (containsIn(det['determinated'], closure) or containsIn(closure, det['determinated']) )
+                    #print('CLOSURE ',j,'+: ',closure, ' - CONTAINED: ', contained)
+                    resultStr = resultStr + '\n'.join(str(i) for i in closure)
+                    if(contained):
+                        resultStr = resultStr +  ' - CONTAINED: TRUE'
+                    else:
+                        resultStr = resultStr +  ' - CONTAINED: FALSE'
+
+                    if contained:
+                        temp_determinant.append(j)
+                if len(temp_determinant) > 0:
+                    l2.append({ "determinant": temp_determinant.copy(), "determinated": det['determinated'] })  
+            elif len(det['determinant']) > 2:
+                
+                #Hallar objecto extraño para determinant de más de 3
+                for L in range(0, len(det['determinant'])+1):
+                    for subset in itertools.combinations(det['determinant'], L):
+                        if len(subset) > 0 and len(subset) < len(det['determinant']):
+                            attributesCombinations.append(list(subset))
+                #print('\n', attributesCombinations, '\n')
+                resultStr = resultStr + '\n'
+                resultStr = resultStr + ''.join(str(i) for i in attributesCombinations) 
+
+                result = []
+                
+                for attr in reversed(attributesCombinations):
+
+                    closure = getClosure(attr, converted_determinants.copy())
+                    contained = containsIn(det['determinated'], closure)
+                    if contained:
+                        result = list(set(det['determinant']) - (set(det['determinant']) - set(attr)))
+                        break
+                    #print(list(set(det['determinant']) - set(attr))) 
+                    resultStr = resultStr + '\n'+list(set(det['determinant']) - set(attr))
+                #print(result)
+                resultStr = resultStr + ''.join(str(i) for i in attributesCombinations) 
+                l2.append({ "determinant": result, "determinated": det['determinated'] })  
+            else:
+                l2.append(det);
+                        
+        #print('\n', 'L2 = \n ', l2, '\n')         
+        resultStr = resultStr + '\n'
+        resultStr = resultStr + '\n'.join(str(i) for i in l2)
+
+        l3 = []
+        
+        temp_determinants = l2.copy()
+        
+        #Find redundants functional Dedendencies
+        for det in l2:
+            #print(det)
+            resultStr = resultStr + ''.join(str(i) for i in det)
+            temp_determinants.remove(det)
+            closure = getClosure(det['determinant'], temp_determinants.copy())
+            temp_determinants.append(det)
+            contained = (containsIn(det['determinated'], closure) or containsIn(closure, det['determinated']) )
+            #print('CLOSURE = ', closure)
+            #print('CONTAINED= ', contained)
+            resultStr = resultStr + '\nCLOSURE = '
+            resultStr = resultStr + '\n'.join(str(i) for i in closure)
+            if(contained):
+                resultStr = resultStr + '\nCONTAINED = TRUE'
+            else:
+                resultStr = resultStr + '\nCONTAINED = FALSE'
+
+
+            if not contained:
+                l3.append(det)
+            else:
+                temp_determinants.remove(det)
+                
+        #print('L3 = ', l3, '\n')
+        outputStr = '\n'.join(str(i) for i in l3)
+        resultStr = resultStr + '\n'.join(str(i) for i in l3)
+        setOutputToFile(resultStr)
+
+        #return outputStr
+        return l3
+
+def setOutputToFile(datos):
+    with open('output.json', 'w') as file:
+        json.dump(datos, file)
+        
+def containsIn(valueToVerify, setToVerify):
+    for value in valueToVerify:
+        if value not in setToVerify:
+            return False    
+    return True
+
+def belongsFD(determined, closure):    
+   determinedList = determined.split(',')            
+   return containsIn(determinedList, closure)
+
+def getClosure(ct, determinants_temp):
+    result = ct
+    prev_result = []
+
+    while result != prev_result:
+        
+        prev_result = result
+
+        for idxt, t in enumerate(prev_result):            
+            #determinants_to_check = determinants_temp.copy();
+        
+            for j in reversed(determinants_temp):
+
+                if t in j['determinant']:
+
+                    if containsIn(j['determinant'], prev_result):
+                        result = result + list(set(j['determinated']) - set(result))
+                        determinants_temp.remove(j)
+                
+            #determinants_temp = determinants_to_check    
+    return sorted(result)
+
+def getClosureL(attributes, determinants):
+    
+    attributesCombinations = []
+    result_l = []
+            
+    for L in range(0, len(attributes)+1):
+                for subset in itertools.combinations(attributes, L):
+                    if len(subset) > 0:
+                        attributesCombinations.append(list(subset))
+    #print('Generando L+...')
+    for ct in attributesCombinations:
+        result_l.append({",".join(ct): getClosure(ct, determinants.copy())})
+        
+    return result_l
+
+def splitDeterminantsWithLenGreaterThan1(determinant, dictionaryToInsert):
+    
+    for det in determinant['determinated']:
+        dictionaryToInsert.append({"determinant": determinant['determinant'], "determinated": [det]})
+
+def fCalculateKeys(attributes,determinants):
+    L3 = []
+    determinant = []
+    determinated= []
+    Z = []
+    resultStr = ''
+    L3 = calculateMinimalCoating(determinants)
+    
+    #Se identifican y ordenan los determinantes y determinados
+    for det in L3:
+        if det['determinated'] not in determinated:
+            determinated.append(det['determinated'][0])
+        if det['determinant'] not in determinant:
+            determinant.append(det['determinant'][0])
+    
+    #Se calcula Z
+    for ai in sorted(attributes):
+        if ai not in sorted(determinated):
+            Z.append(ai)   
+            
+    zplus = getClosure(Z,L3.copy())
+    
+    #print('L3= ',L3)
+    resultStr = resultStr+'Z= '+",".join(Z)+'\n'    
+    resultStr = resultStr+'Z+= '+",".join(zplus)+'\n'
+    
+    #Se evalua si Z+ es una llave candidata
+    
+    
+    setOutputToFile(resultStr)
+    return resultStr
---
+++ Main.pyw
@@ -0,0 +1,142 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep  8 15:18:01 2018
+
+@author: Oscar,Fernando
+"""
+import sys
+from menu import *
+from PyQt5.QtWidgets import QDialog, QApplication, QWidget, QMainWindow, QFileDialog
+from PyQt5 import QtCore
+from Funciones import selectTypeInput,getInputFromFile,getDeterminants,getAttributes,setOutputToFile,belongsFD,getClosureL,getClosure,calculateMinimalCoating,fCalculateKeys
+import itertools
+
+class Main(QMainWindow, Ui_MainWindow):
+        
+    def __init__(self, parent=None):
+        QWidget.__init__(self,parent)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)    
+        
+        self.ui.mdiArea.addSubWindow(self.ui.subwindow_one)
+        self.ui.mdiArea.addSubWindow(self.ui.subwindow_two)
+        self.ui.mdiArea.addSubWindow(self.ui.subwindow_three)
+        self.ui.mdiArea.addSubWindow(self.ui.subwindow_four)
+        self.ui.mdiArea.addSubWindow(self.ui.subwindow_five)
+
+        self.ui.buttonOpen.clicked.connect(self.showComboBox)
+        
+        self.ui.buttonClousureCalculate_one.clicked.connect(self.calculateClousureL)        
+        self.ui.buttonClousureCalculate_two.clicked.connect(self.calculateClousureLManual)
+        self.ui.buttonMinimalCoating_four.clicked.connect(self.calculateMinimalCoating)
+        self.ui.buttonCalculateKeys.clicked.connect(self.calculateKeys)
+
+
+        self.ui.buttonBelongsL.clicked.connect(self.belongsToClousure)                
+        self.ui.buttonUploadFile_one.clicked.connect(self.openFile)   
+        self.ui.buttonUploadFile_four.clicked.connect(self.openFile)   
+        self.ui.buttonUploadFile_five.clicked.connect(self.openFile)   
+
+        self.ui.mdiArea.hide()
+        
+    def showComboBox(self):
+        option = self.ui.comboBoxMain.currentIndex()        
+        self.ui.mdiArea.show()
+
+        if option == 0:
+            self.closeAllWindows()            
+            self.ui.subwindow_one.showMaximized()
+        elif option == 1:
+            self.closeAllWindows()
+            self.ui.subwindow_two.showMaximized()
+        elif option == 2:
+            self.closeAllWindows()
+            self.ui.subwindow_three.showMaximized()
+        elif option == 3:
+            self.closeAllWindows()
+            self.ui.subwindow_four.showMaximized()
+        elif option == 4:
+            self.closeAllWindows()
+            self.ui.subwindow_five.showMaximized()
+
+    def closeAllWindows(self):
+        self.ui.labelFile_one.setText("buscar archivo JSON")
+        self.ui.labelFile_three.setText("buscar archivo JSON")
+        self.ui.subwindow_one.hide()
+        self.ui.subwindow_two.hide()
+        self.ui.subwindow_three.hide()
+        self.ui.subwindow_four.hide()
+        self.ui.subwindow_five.hide()
+    
+    def openFile(self):        
+        #file, _ = QFileDialog.getOpenFileName(self, 'Buscar Archivo', QtCore.QDir.homePath(), "All Files (*);;Text Files (*.txt)")
+        fileRoute, _ = QFileDialog.getOpenFileName(self, 'Buscar Archivo', QtCore.QDir.homePath(), "All Files (*.json)")
+        if fileRoute:
+            self.ui.labelFile_one.setText(fileRoute)              
+            self.ui.labelFile_three.setText(fileRoute)     
+            self.ui.labelFile_four.setText(fileRoute)   
+            self.ui.labelFile_five.setText(fileRoute)              
+
+    def calculateClousureL(self):
+        jsonInput = getInputFromFile(self.ui.labelFile_one.text())
+        self.ui.plainTextEditL_one.clear()
+        
+        if jsonInput is not None:
+            determinants = getDeterminants(jsonInput['L'])
+            attributes = getAttributes(jsonInput)
+            result = getClosureL(attributes, determinants)
+            strResult = ''.join([str(i) for i in result])
+            self.ui.plainTextEditL_one.appendPlainText(strResult)
+            setOutputToFile(strResult)
+    
+    def calculateClousureLManual(self):
+        self.ui.plainTextEditClousure.clear()
+        attributes = self.ui.lineEditT.text()
+        determinants = self.ui.lineEditL.text() 
+        determinants = getDeterminants(determinants.split(';'))
+        result = getClosureL(attributes.split(','), determinants)
+        strResult = ''.join([str(i) for i in result])
+        self.ui.plainTextEditClousure.appendPlainText(strResult)     
+        setOutputToFile(strResult)
+        
+    def belongsToClousure(self):
+        self.openFile()
+        jsonInput = getInputFromFile(self.ui.labelFile_three.text())
+        determinant =self.ui.lineEditDeterminant.text()
+        determinant = determinant.strip()
+        determined = self.ui.lineEditDetermined.text()
+        determined = determined.strip() 
+        determinants = getDeterminants(jsonInput['L'])       
+        closure = getClosure(determinant.split(','), determinants);
+        result = belongsFD(determined, closure)
+        
+        if result:
+            self.ui.labelResult.setText("La dependencia pertenece al cierre L+")
+        else:
+            self.ui.labelResult.setText("La dependencia no pertenece al cierre L+")
+
+    def calculateMinimalCoating(self):
+        jsonInput = getInputFromFile(self.ui.labelFile_four.text())
+        self.ui.plainTextEditL_four.clear()
+        determinants = getDeterminants(jsonInput['L'])       
+        result = calculateMinimalCoating(determinants)
+        self.ui.plainTextEditL_four.appendPlainText(result)     
+
+    def calculateKeys(self):
+        jsonInput = getInputFromFile(self.ui.labelFile_five.text())
+        self.ui.plainTextEditL_five.clear()
+        
+        if jsonInput is not None:
+            determinants = getDeterminants(jsonInput['L'])
+            attributes = getAttributes(jsonInput)
+            #result = getClosureL(attributes, determinants)
+            #strResult = ''.join([str(i) for i in result])
+            self.ui.plainTextEditL_five.appendPlainText(fCalculateKeys(attributes,determinants))
+            #setOutputToFile(strResult)
+        
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    application = Main()
+    application.show()
+    sys.exit(app.exec_())
+        
